069047328aa51a136c1a9c2901fa4e23b9af92ae
jaebradley <jae.b.bradley@gmail.com>
2018-02-17 07:59:09 -0500

    feat(executables): add flight search command

---
+++ executables/flights-search.js
@@ -0,0 +1,9 @@
+const executeSearch = async () => {
+  try {
+    // add search method
+  } catch (e) {
+    console.error(`Rut ro - there was an error: ${e}`); // eslint-disable-line no-console
+  }
+};
+
+executeSearch();
---
+++ executables/flights.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import program from 'commander';
+
+import pkg from '../package.json';
+
+program.version(pkg.version)
+  .description('Search flights from the command line')
+  .command('search', 'Search flights')
+  .parse(process.argv);
